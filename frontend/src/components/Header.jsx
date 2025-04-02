66a923ce9cefa8cdfc2f92191e8036adc988c30a
jessicaghunt <huntjessicag@gmail.com>
2025-04-02 12:37:41 -0400

    add a login function

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 // import Siderbar from "./src/styles/Siderbar.css"
 const Heading = () => {
+    const [popUp, setPopUp] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const runLogin = () => {
+        if (username === "admin" && password === "12345678") {
+            setPopUp(false);
+            navigate("/login");
+        }else{
+            setError("Galloping Billy Goats!! Incorrect login information. Try Again")
+        }
+        
+    }
     return (
+        <>
         <div className="heading-bar">
             <div className="logo">
                 <Link to="/"><div style={{ position: 'relative', fontFamily: 'sans-serif'}}>
@@ -19,17 +36,36 @@ const Heading = () => {
                                 <div className="link-word">Create Account</div>
                             </div></Link>
                         </li>
-                        <li>
-                        <Link to="/login" title="Manager Login">
-                            <div style={{ position: 'relative', fontFamily: 'sans-serif'}}>
+                        <li> <button onClick={() => setPopUp(true)} className="login-button">Login</button>
+                        {/* <Link to="/login" title="Manager Login"> */}
+                            {/* <div style={{ position: 'relative', fontFamily: 'sans-serif'}}> */}
                             {/* <img src="../test-image.png" className="logo" /> */}
-                                <div className="link-word">Login</div>
-                            </div></Link>
+                                {/* <div className="link-word">Login</div> */}
+                            {/* </div></Link> */}
                             </li>
                     </ul>
                 </nav>
             </div>
+            </div>
+
+            {popUp && (
+                <div className="popup-overlay">
+                    <div className="popup-container">
+                        <h2>Login</h2>
+                        {error && <p className="error">{error}</p>}
+                        <input
+                            type="text" placeholder="Trail Username" value={username} onChange={(e) => setUsername(e.target.value)}
+                            />
+                        <input 
+                            type="password" placeholder="Whats the Password?" value={password} onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button onClick={runLogin}>Submit</button>
+                            <button onClick={() => setPopUp(false)}>Close</button>
+                    </div>
         </div>
+            )}
+            
+            </>
     );
 };
 
